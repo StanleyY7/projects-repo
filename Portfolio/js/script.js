9c7d8f9e2a45b38a8c414760d0a8a91de83b2e1e
StanleyY7 <stanleyyu7@gmail.com>
2022-12-12 21:31:18 +1100

    95% complete

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,5 +1,25 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".nav-links a");
+
 function copyright() {
   document.write("&copy" + new Date().getFullYear() + " Stanley Yu ");
 }
 
 window.onload = copyright();
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
